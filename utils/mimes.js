9ce825efcfcfb3c3ab9bc6ee174dd2c3f3c8705c
yaozhibo <yyaozhibo@163.com>
2019-10-31 10:00:55 +0800

    first commit

---
+++ utils/mimes.js
@@ -0,0 +1,10 @@
+// 附件 1.1
+// 班级：软一
+// 姓名：姚志博
+export default {
+    'jpg': 'image/jpeg',
+    'png': 'image/png',
+    'gif': 'image/gif',
+    'svg': 'image/svg+xml',
+    'psd': 'image/photoshop'
+}
